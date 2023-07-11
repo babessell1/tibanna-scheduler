889d2b6d19bfb4a061383e038b5af9334e9fbece
babessell1 <babessell@gmail.com>
2023-07-11 13:20:21 -0400

    [fix] remove path from subject

--- py/helpers.py
+++ py/helpers.py
@@ -48,7 +48,7 @@ def get_subject_completed_set(outbucket, prefix):
                 #subj = extract_subjects(str(obj['Key']))
                 subj = str(obj['Key']).split(".tar")[0].split("___")
                 for s in subj:
-                    completed_set.add(extract_subjects(s))
+                    completed_set.add(extract_subjects(s).split["/"][0])
 
     return completed_set
 
