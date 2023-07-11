e6c4c6f68df0cfdb19e56bda21732f5da396d009
babessell1 <babessell@gmail.com>
2023-07-11 13:26:07 -0400

    [fix] get_subject split off path

--- py/helpers.py
+++ py/helpers.py
@@ -48,7 +48,7 @@ def get_subject_completed_set(outbucket, prefix):
                 #subj = extract_subjects(str(obj['Key']))
                 subj = str(obj['Key']).split(".tar")[0].split("___")
                 for s in subj:
-                    completed_set.add(extract_subjects(s).split["/"][0])
+                    completed_set.add((lambda x: x.split('/')[-1])(extract_subjects(s)))
 
     return completed_set
 
