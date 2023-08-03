94aca2da23d770addba2373db0f3eade56c76491
babessell1 <babessell@gmail.com>
2023-08-03 17:44:10 -0400

    [debug] remove prints

--- py/helpers.py
+++ py/helpers.py
@@ -68,10 +68,8 @@ def resolve_inputs(csv_file, batch_size, outbucket, cores_per_inst, prefix, allo
         reader = csv.DictReader(file)
         locations = []
         completed_set = get_subject_completed_set(outbucket, prefix=prefix[1:]) if not allow_existing else {}
-        print(completed_set)
 
         for row in reader:
-            print(row['Subject'])
             if not any(row['Subject'] in item for item in completed_set):
                 location = row['location']
                 if exclude_failed and file_in_failed(row['Subject'], try_again=try_again):
