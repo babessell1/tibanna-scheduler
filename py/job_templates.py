1309fa4ca401abccd5aea491a819edeb0ae0c28d
babessell1 <babessell@gmail.com>
2023-08-16 16:22:08 -0400

    [fix] melt log bucket

--- py/job_templates.py
+++ py/job_templates.py
@@ -111,7 +111,7 @@ def get_job_templates(inbucket, outbucket, inputs, inputs_idx, ebs_size, instanc
                 "instance_type": {json.dumps(instance_types)},
                 "EBS_optimized": true,
                 "password": "",
-                "log_bucket": "marcotte-out",
+                "log_bucket": "niagads-melt",
                 "spot_instance": true,
                 "key_name": "big-wgs-key"
             }}
