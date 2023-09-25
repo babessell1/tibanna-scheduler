757f87c8c3152f52a6ca3d99a3ed3a2f5260e844
babessell1 <babessell@gmail.com>
2023-09-25 14:56:21 -0400

    [change] delete rows from file if they are called

--- scratch.bash
+++ scratch.bash
@@ -6,12 +6,13 @@ python py/scheduler.py \
     --instance-cpus 2 \
     --requester-pays \
     --rerun-failed \
+    --use-slurm \
     --instance-types "c6i.large" \
     --job-key "call_strling"  \
-    --batch-size 2 \
+    --batch-size 250 \
     --csv "ADNI_MIA_PR1066_VAN_StEP_manifest.csv" \
-    --mode "launch"  \
-    --id test_1_strling_ADNI_MIA_PR1066_VAN_StEP
+    --mode "download"  \
+    --id "1_strling_ADNI_MIA_PR1066_VAN_StEP"
 
 python py/scheduler.py \
     --ebs-size 70 \
@@ -21,12 +22,13 @@ python py/scheduler.py \
     --instance-cpus 2 \
     --requester-pays \
     --rerun-failed \
+    --use-slurm \
     --instance-types "m7i-flex.large" \
     --job-key "call_ehdn"  \
-    --batch-size 2 \
+    --batch-size 250 \
     --csv "ADNI_MIA_PR1066_VAN_StEP_manifest.csv"  \
     --mode "launch" \
-    --id test_1_ehdn_ADNI_MIA_PR1066_VAN_StEP
+    --id "1_ehdn_ADNI_MIA_PR1066_VAN_StEP"
 
 
-     4366
+     
