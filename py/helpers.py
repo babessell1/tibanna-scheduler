4b985941e9820179dbb8d9852bce7bea11bd0857
babessell1 <babessell@gmail.com>
2023-10-07 20:33:06 -0400

    fix index

--- py/helpers.py
+++ py/helpers.py
@@ -111,7 +111,7 @@ def resolve_inputs(csv_file, csv_start, csv_end, remove_from_csv,
 
     print(csv_start, csv_end)
 
-    for i, row in enumerate(rows[csv_start:csv_end+1], 1):
+    for i, row in enumerate(rows[csv_start:csv_end+1]):
         print(i + csv_start, row)
         if not any(row['Subject'] in item for item in completed_set):
             location = row['location']
@@ -127,7 +127,7 @@ def resolve_inputs(csv_file, csv_start, csv_end, remove_from_csv,
 
         if len(locations) >= batch_size:
             break
-        elif bind_location and i+1 >= csv_end - csv_start:
+        elif bind_location and i >= csv_end - csv_start:
             break
 
     print("locs: ", len(locations))
